53ee2f21aa88163a6662ce81ebf834966fef5841
hesa <hesa@sandklef.com>
2016-05-03 10:21:55 +0200

    Commit after restore

---
+++ app/src/main/java/com/sandklef/coachapp/activities/BootStrapApp.java
@@ -0,0 +1,7 @@
+package com.sandklef.coachapp.activities;
+
+/**
+ * Created by hesa on 2016-04-28.
+ */
+public class BootStrapApp {
+}
---
+++ app/src/main/java/com/sandklef/coachapp/model/LogMessage.java
@@ -0,0 +1,7 @@
+package com.sandklef.coachapp.model;
+
+/**
+ * Created by hesa on 2016-04-21.
+ */
+public class LogMessage {
+}
